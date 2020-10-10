2f5c2eca2d49dfca59762db65c8f4370104c9868
ElliotDewdney <60190829+ElliotDewdney@users.noreply.github.com>
2020-10-10 20:23:26 +0100

    working interpriter

---
+++ src/BareBones.java
@@ -0,0 +1,173 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class BareBones {
+
+    static class BareBonesRuntime{
+
+        private static class conditionalJump{
+            public interface booleanCondition{
+                boolean checkCondition();
+            }
+            private booleanCondition condition;
+            private int lineReference;
+            public conditionalJump(int lineReference, booleanCondition condition){
+                this.lineReference = lineReference;
+                this.condition = condition;
+            }
+            public boolean evaluate(){
+                return condition.checkCondition();
+            }
+            public int getLineNumber(){
+                return lineReference;
+            }
+        }
+
+        public static class programLine{
+            private instructions action;
+            private String[] parameters;
+            public programLine(instructions action, String[] parameters){
+                this.action = action;
+                this.parameters = parameters;
+            }
+            void runLine(BareBonesRuntime runtime){
+                action.action.action(runtime, parameters);
+            }
+        }
+
+        private Stack<conditionalJump> conditionalJumps = new Stack<>();
+        private HashMap<String, Integer> CurrentVars = new HashMap<>();
+        private static HashMap<String, instructions> instructionsHashMap = new HashMap<>();
+        private static HashMap<String, comparators> comparatorsHashMap = new HashMap<>();
+        private int programCounter;
+        private programLine[] programLines;
+        private String[] FinalVarsNames;
+        private int[] FinalVarsValues;
+
+        public BareBonesRuntime(String[] lines){
+            programLine[] programLines = new programLine[lines.length];
+            for(int i = 0; i < lines.length; i++){
+                String[] line = lines[i].split("\\w");
+                String[] parameters = new String[line.length-1];
+                for(int j = 0; j < parameters.length; j++) parameters[j] = line[j+1];
+                programLines[i] = new programLine(instructionsHashMap.get(line[0]), parameters);
+            }
+            constructor(programLines);
+        }
+
+        public BareBonesRuntime(programLine[] lines){
+            constructor(lines);
+        }
+
+        private void constructor(programLine[] lines){
+            programLines = lines;
+            while(programCounter<programLines.length){
+                programLines[programCounter].runLine(this);
+                programCounter++;
+            }
+            FinalVarsNames = new String[CurrentVars.size()];
+            FinalVarsNames = CurrentVars.keySet().toArray(FinalVarsNames);
+            Integer[] temp  = new Integer[CurrentVars.size()];
+            temp = CurrentVars.values().toArray(temp);
+            FinalVarsValues = new int[temp.length];
+            for(int i = 0; i < temp.length; i++) FinalVarsValues[i] = temp[i];
+        }
+
+        public String[] getVariablesNames(){
+            return FinalVarsNames;
+        }
+        public int[] getVariablesValues(){
+            return FinalVarsValues;
+        }
+
+        private interface instruction {
+            void action(BareBonesRuntime runtime, String[] parameters);
+        }
+        private interface comparator {
+            boolean compare(int x, int y);
+        }
+
+        //use of enums to define language behavior
+        private enum comparators{
+            NOT("not", (x,y) -> x!=y),
+            IS("is", (x,y) -> x==y),
+            GREATER("greater", (x,y) -> x>y),
+            LESSER("lesser", (x,y) -> x<y)
+
+            ;
+            private String name;
+            private comparator action;
+            comparators(String name,comparator action){
+                this.name = name;
+                this.action = action;
+                comparatorsHashMap.put(name, this);
+            }
+            boolean compare(int x, int y){
+                return action.compare(x,y);
+            }
+
+        }
+
+        private enum instructions{
+
+            INCREMENT("incr", (runtime, parameters) -> {
+                runtime.CurrentVars.replace(parameters[0], runtime.CurrentVars.get(parameters[0])+1);
+            }),
+
+            DECREMENT("decr", (runtime, parameters) -> {
+                runtime.CurrentVars.replace(parameters[0], runtime.CurrentVars.get(parameters[0])-1);
+            }),
+
+            CLEAR("clear", (runtime, parameters) -> {
+                if(runtime.CurrentVars.containsKey(parameters[0]))
+                    runtime.CurrentVars.replace(parameters[0], 0);
+                else
+                    runtime.CurrentVars.put(parameters[0], 0);
+            }),
+
+            WHILE("while", (runtime, parameters) -> runtime.conditionalJumps.push( new conditionalJump(runtime.programCounter, () -> comparatorsHashMap.get(parameters[1]).compare(runtime.CurrentVars.get(parameters[0]), Integer.parseInt(parameters[2]))))),
+
+            END("end", (runtime, parameters) -> {
+                if(runtime.conditionalJumps.peek().evaluate())
+                    runtime.programCounter = runtime.conditionalJumps.peek().getLineNumber();
+                else
+                    runtime.conditionalJumps.pop();
+            }),
+
+            // new instructions are now easily added
+            // this is done by creating a anonymous instance of the interface instruction
+            /*
+            Lets say you want to add a instruction that squares the number held in the variable
+            it would look like:
+            square X
+
+            to create it would wold just write here :
+            SQUARE("square", (runtime, parameters) -> runtime.CurrentVars.replace(parameters[0], math.pow(2,runtime.CurrentVars.get(parameters[0])))),
+
+            and that would add the instruction to the language
+             */
+            ;
+
+            private String name;
+            private instruction action;
+            instructions(String name,instruction action){
+                this.name = name;
+                this.action = action;
+                instructionsHashMap.put(name, this);
+            }
+            public void runAction(BareBonesRuntime runtime, String[] parameters){
+                action.action(runtime, parameters);
+            }
+        }
+
+    }
+
+    public static void main(String args[]){
+        BareBonesRuntime temp = new BareBonesRuntime(new BareBonesRuntime.programLine[]{
+                new BareBonesRuntime.programLine(BareBonesRuntime.instructions.CLEAR, new String[]{"X"}),
+                new BareBonesRuntime.programLine(BareBonesRuntime.instructions.INCREMENT, new String[]{"X"}),
+                new BareBonesRuntime.programLine(BareBonesRuntime.instructions.INCREMENT, new String[]{"X"}),
+        });
+        System.out.println(temp.getVariablesNames()[0] + " -> " + temp.getVariablesValues()[0]);
+    }
+}
